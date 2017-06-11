fe818fe7d5e777aae5374845987888d99d0efa89
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2017-06-11 10:19:40 +0300

    add product route and model

---
+++ app/models/product.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    size: {
+        type: [String]
+    },
+    color: {
+        type: [String]
+    },
+    quantity: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    price: {
+        type: String
+    }
+});
+
+
+module.exports = mongoose.model('Product', ProductSchema);
---
+++ app/routes/product.js
@@ -0,0 +1,14 @@
+/* Node modules */
+var express = require('express');
+
+/* Static files */
+var Product = require('../models/product');
+
+
+module.exports = function() {
+	let productRoute = express.Router();
+
+	
+	
+	return productRoute;
+}
